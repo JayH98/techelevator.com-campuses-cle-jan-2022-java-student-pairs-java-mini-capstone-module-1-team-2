622a36dc06ec67c3de4ce8e5591a278cf535b167
Mikey Stewart <mjstewa92@gmail.com>
2022-02-10 22:52:39 -0500

    minor changes to structure

--- capstone/src/main/java/com/techelevator/view/Gum.java
+++ capstone/src/main/java/com/techelevator/view/Gum.java
@@ -1,11 +1,15 @@
 package com.techelevator.view;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Gum extends Vendables {
 
     Map<String, Double> gum = new HashMap<String, Double>();
+
+    List<Gum> gums = new ArrayList<>();
 
     public Gum(String itemName, double itemPrice, String itemSound) {
         super(itemName, itemPrice, itemSound);
