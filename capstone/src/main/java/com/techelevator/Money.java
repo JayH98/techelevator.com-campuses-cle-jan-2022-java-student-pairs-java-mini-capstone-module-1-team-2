b75a81374e2bc14597b26d1fc631865a22659491
Kyle Youngblood <kyoungbl21@gmail.com>
2022-02-10 19:48:39 -0500

    Saving Progress

--- capstone/src/main/java/com/techelevator/Money.java
+++ capstone/src/main/java/com/techelevator/Money.java
@@ -3,7 +3,22 @@ package com.techelevator;
 public class Money {
 
 
-Inventory inventory = new Inventory();
+    private final double dollar = 1.00;
+    private final double twoDollar = 2.00;
+    private final double fiveDollars = 5.00;
+    private final double tenDollars = 10.00;
+
+    private final double changeNickel = 0.05;
+    private final double changeDime = 0.10;
+    private final double changeQuarter = 0.25;
+
+
+    /*
+
+
+
+
+     */
 
 
 
