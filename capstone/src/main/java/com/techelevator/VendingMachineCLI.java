ec36c2227a63695c381e0440ff100595dee2caa6
Mikey Stewart <mjstewa92@gmail.com>
2022-02-10 21:16:57 -0500

    pushing changes

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -2,27 +2,46 @@ package com.techelevator;
 
 import com.techelevator.view.Menu;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class VendingMachineCLI {
+
+	private List<String[]> stuff = new ArrayList<>();
+	Inventory inventory = new Inventory();
+
+	public void setStuff(List<String[]> parsedInventory) {
+		this.stuff = inventory.getParsedInventory();
+	}
+
+	String[] strings = {"hello", "goodbye"};
+
+
 
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
 
 	private Menu menu;
-	
+
 
 	public VendingMachineCLI(Menu menu) {
 		this.menu = menu;
 	}
 
-	Inventory inventory = new Inventory();
+
 
 	public void run() {
 		while (true) {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-				System.out.println("cool items");
+				for (String[] stringOfItems : stuff){
+					System.out.println(stringOfItems.toString());
+				}
+
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
 			}
