3024ad4498f1aba8ffe0fdc235f6de03c47458c5
Kyle Youngblood <kyoungbl21@gmail.com>
2022-02-13 13:19:14 -0500

    Save -KY

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -3,7 +3,6 @@ package com.techelevator;
 import com.techelevator.view.Menu;
 import com.techelevator.view.Vendables;
 
-import javax.swing.*;
 import java.util.*;
 
 public class VendingMachineCLI {
@@ -15,15 +14,15 @@ public class VendingMachineCLI {
     //	Map<item location, item price/quantity/sound/name>
     Map<String, Vendables> sale = inventory.getMapOfItems(); // = Map of items in vending machine
 
-    private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
-    private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
-    private static final String MAIN_MENU_OPTION_LEAVE = "Leave";
+    private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Menu Items";
+    private static final String MAIN_MENU_OPTION_PURCHASE = "Make a Purchase";
+    private static final String MAIN_MENU_OPTION_LEAVE = "Exit";
     private static final String[] MAIN_MENU_OPTIONS = {MAIN_MENU_OPTION_DISPLAY_ITEMS,
             MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_LEAVE};
 
-    private static final String PURCHASE_MENU_OPTION_FEED_MONEY = "Feed Money";
-    private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select Product";
-    private static final String PURCHASE_MENU_OPTION_FINISH_TRANSACTION = "Finish Transaction";
+    private static final String PURCHASE_MENU_OPTION_FEED_MONEY = "Please Feed Money";
+    private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select a Product";
+    private static final String PURCHASE_MENU_OPTION_FINISH_TRANSACTION = "Complete Transaction";
     private static final String[] PURCHASE_MENU_OPTIONS = {PURCHASE_MENU_OPTION_FEED_MONEY,
             PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_FINISH_TRANSACTION};
 
@@ -32,8 +31,6 @@ public class VendingMachineCLI {
     MoneyTransaction money = new MoneyTransaction();
     Customer customer = new Customer();
     Map<String, Integer> newMapForQuantity = customer.getItemsCustomerHas();
-
-
 
 
     public VendingMachineCLI(Menu menu) {
@@ -74,7 +71,6 @@ public class VendingMachineCLI {
                         if (customer.getItemQuantity(selection) == null) {
                             customer.setInitialItems(selection);
                         }
-                        //TODO figure out why reseting to 5
                         if (customer.getItemQuantity(selection) >= 0 && customer.getItemQuantity(selection) < 5) {
                             if (money.getBalance() >= itemPicked.getItemPrice()) {
                                 System.out.println();
@@ -84,7 +80,6 @@ public class VendingMachineCLI {
                                 money.transaction(moneyToSubtract);
                                 System.out.println(itemPicked.getItemSound());
                                 System.out.println(customer.getItemQuantity(selection));
-
 
                             } else {
                                 System.out.println("You need to enter more money.");
@@ -98,8 +93,6 @@ public class VendingMachineCLI {
                         System.out.println();
                         System.out.println("Change returned: " + changeForCustomer[0] + " Quarter(s) " + changeForCustomer[1]
                                 + " Dime(s) " + changeForCustomer[2] + " Nickel(s)");
-
-
                         break;
                     }
                 }
